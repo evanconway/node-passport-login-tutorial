36b5d17eee03644094ff8718fc2d15e7e7f1ab50
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-01-21 19:27:21 -0500

    setup views

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.set('view-enginer', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        name: 'Kyle',
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/login', (req, res) => {
+    
+});
+
+app.post('/register', (req, res) => {
+
+});
+
+app.listen(3000);
